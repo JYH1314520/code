b577a3e6dbdd97b70a157992c4f4eed793d0e857
Your Name <email@example.com>
2018-06-07 10:09:31 +0800

    tab-nav

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -75,17 +75,16 @@ layui.config({
 			}
 		})
 	}
-
+	//判断字段数据是否存在
+	function nullData(data){
+		if(data == '' || data == undefined || data == null){
+			return "未定义";
+		}else{
+			return data;
+		}
+	}
 	//填充数据方法
  	function fillParameter(data){
- 		//判断字段数据是否存在
- 		function nullData(data){
- 			if(data == '' || data == "undefined"){
- 				return "未定义";
- 			}else{
- 				return data;
- 			}
- 		}
  		$(".version").text(nullData(data.version));      //当前版本
 		$(".author").text(nullData(data.author));        //开发作者
 		$(".homePage").text(nullData(data.homePage));    //网站首页
@@ -94,5 +93,4 @@ layui.config({
 		$(".maxUpload").text(nullData(data.maxUpload));    //最大上传限制
 		$(".userRights").text(nullData(data.userRights));//当前用户权限
  	}
-
 })
